c17d4af04918a4b8b4c3b6d6c076568dde5703ae
Wes <averagewizard13@gmail.com>
2021-06-02 19:23:46 -0600

    Use Django settings rather than dotenv

--- rubydashboard/settings/common.py
+++ rubydashboard/settings/common.py
@@ -13,23 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
-from dotenv import load_dotenv
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv("SECRET_KEY"))
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["ruby.deadtired.me", "localhost"]
 
 
 # Application definition
---
+++ rubydashboard/settings/development.py
@@ -0,0 +1,9 @@
+from rubydashboard.settings.common import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+
+SECRET_KEY = 'yemh+q@w#tl3o48l*0_hwnfqvid%*_3=e_2ss46+=j_p52_p#x'
+DEBUG = True
+
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
